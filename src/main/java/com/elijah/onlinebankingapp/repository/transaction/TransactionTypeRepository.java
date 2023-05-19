9fcd42dc16d0a0ed231f91b04174b135a4567712
ElijahUkeme <ukemedmet@gmail.com>
2023-05-19 05:21:36 -0700

    Recent modifications

--- src/main/java/com/elijah/onlinebankingapp/repository/transaction/TransactionTypeRepository.java
+++ src/main/java/com/elijah/onlinebankingapp/repository/transaction/TransactionTypeRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -14,9 +16,9 @@ public interface TransactionTypeRepository extends JpaRepository<TransactionType
 
     List<TransactionType> findByBankAccount(BankAccount bankAccount);
 
-    @Query("select t from TransactionType t where t.bankAccount = :bankAccount and t.transactionDate <= :startDate AND t.transactionDate >= :endDate")
-   List<TransactionType> findByBankAccountAndTransactionDate(BankAccount bankAccount, @Param("startDate") Date startDate,@Param("endDate") Date endDate);
+    @Query("select t from TransactionType t where t.bankAccount = :bankAccount and t.transactionDate >= :startDate AND t.transactionDate <= :endDate")
+   List<TransactionType> findByBankAccountAndTransactionDate(BankAccount bankAccount, @Param("startDate") LocalDate startDate,@Param("endDate") LocalDate endDate);
 
-    @Query("select t from TransactionType t where t.transactionDate <= :startDate and t.transactionDate >= :endDate")
-    List<TransactionType> getTransactionStatement(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
+    @Query("select t from TransactionType t where t.transactionDate >= :startDate and t.transactionDate <= :endDate")
+    List<TransactionType> getTransactionStatement(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
