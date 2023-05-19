c4620dd1ce02e64fba29495fb0a2ac36de2a1d18
ElijahUkeme <ukemedmet@gmail.com>
2023-05-19 05:18:56 -0700

    Some new modifications

--- src/main/java/com/elijah/onlinebankingapp/dto/bank/BankAccountStatementDto.java
+++ src/main/java/com/elijah/onlinebankingapp/dto/bank/BankAccountStatementDto.java
@@ -1,13 +1,23 @@
 package com.elijah.onlinebankingapp.dto.bank;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 
-@Data
 @NoArgsConstructor
+@Data
 public class BankAccountStatementDto {
-    private Date startDate;
-    private Date endDate;
+
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
 }
